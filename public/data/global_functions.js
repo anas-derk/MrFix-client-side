bb4e44e31e754f41e4dbbebde799877b33540685
anas derk <anas.derk2020@gmail.com>
2023-05-29 08:05:07 -0700

    Start Programming Signup Validation

---
+++ public/data/global_functions.js
@@ -0,0 +1,56 @@
+const isEmail = (email) => {
+    return email.match(/[^\s@]+@[^\s@]+\.[^\s@]+/);
+}
+
+const isValidPassword = (password) => {
+
+}
+
+const inputValuesValidation = (inputs) => {
+    // تعريف المصفوفة التي ستخزن الأخطاء
+    let errorList = [];
+    // إنشاء حلقة للمرور على كل المدخلات المرسلة إلى التابع وعمل التحقق المطلوب بناء على القواعد المرسلة
+    for (let input of inputs) {
+        // تخزين القواعد الخاصة بالمُدخل في متغير لاستخدامه لاحقاً
+        let inputRules = input.rules;
+        // التحقق من كون القاعدة داخل كائن القواعد موجودة 
+        if (typeof inputRules.isRequired !== "undefined") {
+            // التحقق من أنّ القاعدة محققة ، وفي حالة لم تكن محققة فإننا نضيف الخطأ إلى مصفوفة الأخطاء
+            if (input.value.length === 0) {
+                errorList[input.name] = inputRules.isRequired.msg;
+                // في حالة وجود خطأ نقوم بتجاهل كل التعليمات اللاحقة داخل التكرار الحالي للحلقة والانتقال إلى التكرار التالي
+                continue;
+            }
+        }
+        // التحقق من كون القاعدة داخل كائن القواعد موجودة 
+        if (typeof inputRules.isEmail !== "undefined") {
+            // التحقق من أنّ القاعدة محققة ، وفي حالة لم تكن محققة فإننا نضيف الخطأ إلى مصفوفة الأخطاء
+            if (!isEmail(input.value)) {
+                errorList[input.name] = inputRules.isEmail.msg;
+                // في حالة وجود خطأ نقوم بتجاهل كل التعليمات اللاحقة داخل التكرار الحالي للحلقة والانتقال إلى التكرار التالي
+                continue;
+            }
+        }
+        // التحقق من كون القاعدة داخل كائن القواعد موجودة 
+        if (typeof inputRules.minLength !== "undefined") {
+           // التحقق من أنّ القاعدة محققة ، وفي حالة لم تكن محققة فإننا نضيف الخطأ إلى مصفوفة الأخطاء
+            if (input.value.length < inputRules.minLength.value) {
+                errorList[input.name] = inputRules.minLength.msg;
+                // في حالة وجود خطأ نقوم بتجاهل كل التعليمات اللاحقة داخل التكرار الحالي للحلقة والانتقال إلى التكرار التالي
+                continue;
+            }
+        }
+        // التحقق من كون القاعدة داخل كائن القواعد موجودة 
+        if (typeof inputRules.maxLength !== "undefined") {
+            // التحقق من أنّ القاعدة محققة ، وفي حالة لم تكن محققة فإننا نضيف الخطأ إلى مصفوفة الأخطاء
+            if (input.value.length > inputRules.maxLength.value) {
+                errorList[input.name] = inputRules.maxLength.msg;
+                // في حالة وجود خطأ نقوم بتجاهل كل التعليمات اللاحقة داخل التكرار الحالي للحلقة والانتقال إلى التكرار التالي
+                continue;
+            }
+        }
+    }
+    return errorList;
+}
+
+export default { isEmail,  inputValuesValidation };
--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -2,20 +2,99 @@ import Head from 'next/head';
 import Header from '@/components/Header';
 import { useEffect, useState } from 'react';
 import { FiUserPlus } from "react-icons/fi";
+import global_functions from '../../../public/data/global_functions';
 
 export default function Signup() {
 
-    useEffect(() => {
+    const [firstAndLastName, setFirstAndLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [mobilePhone, setMobilePhone] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [gender, setGender] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [city, setCity] = useState("");
+    const [address, setAddress] = useState("");
+    const [errors, setErrors] = useState([]);
 
+    const createAccount = (e) => {
+        e.preventDefault();
+        setErrors([]);
+        let errorList = global_functions.inputValuesValidation(
+            [
+                {
+                    name: "firstAndLastName",
+                    value: firstAndLastName,
+                    rules: {
+                        isRequired: {
+                            msg: "عذراً ، لا يجب أن يكون الحقل فارغاً !!",
+                        },
+                        minLength: {
+                            value: 5,
+                            msg: "عذراً ، يجب أن يكون عدد الأحرف على الأقل 4 !!"
+                        },
+                        maxLength: {
+                            value: 30,
+                            msg: "عذراً ، يجب أن يكون عدد الأحرف على الأكثر 30",
+                        }
+                    },
+                },
+                {
+                    name: "email",
+                    value: email,
+                    rules: {
+                        isRequired: {
+                            msg: "عذراً ، لا يجب أن يكون الحقل فارغاً !!",
+                        },
+                        isEmail: {
+                            msg: "عذراً ، الإيميل الذي أدخلته غير صالح ، الرجاء إدخال إيميل صالح !!",
+                        },
+                    },
+                },
+                {
+                    name: "mobilePhone",
+                    value: mobilePhone,
+                    rules: {
+                        isRequired: {
+                            msg: "عذراً ، لا يجب أن يكون الحقل فارغاً !!",
+                        },
+                        minLength: {
+                            value: 10,
+                            msg: "عذراً ، يجب أن يكون عدد الأرقام 10",
+                        },
+                        maxLength: {
+                            value: 10,
+                            msg: "عذراً ، يجب أن يكون عدد الأرقام 10",
+                        }
+                    },
+                },
+                {
+                    name: "password",
+                    value: password,
+                    rules: {
+                        isRequired: {
+                            msg: "عذراً ، لا يجب أن يكون الحقل فارغاً !!",
+                        },
+                        isPassword: {
+                            value: password,
+                            msg: "عذراً ، يجب أن يكون عدد أحرف الكلمة 8 على الأقل ولا تحتوي محارف خاصة ، وتحتوي على أحرف",
+                        },
+                    },
+                },
+            ]
+        );
+        setErrors(errorList);
+        // if (!errors) {
+        //     console.log("ee");
+        // } 
+    }
+
+    useEffect(() => {
         let header = document.querySelector("#__next .page-header"),
             pageContent = document.querySelector(".sign-up .page-content");
-
         pageContent.style.minHeight = `calc(100vh - ${header.clientHeight}px)`;
-
         let address = document.querySelector(".sign-up .sign-up-form .address");
-
         address.style.height = `calc(116px + 1.5rem)`;
-
     }, []);
 
     return (
@@ -30,43 +109,87 @@ export default function Signup() {
                 {/* Start Container From Bootstrap */}
                 <div className="container">
                     <h1 className='page-title mb-4'>أهلاً وسهلاً بك في مستر فيكس</h1>
-                    <form className="sign-up-form">
+                    <form className="sign-up-form" onSubmit={createAccount}>
                         {/* Start Grid System From Bootstrap */}
                         <div className="row">
                             {/* Start Column */}
                             <div className="col-md-6">
-                                <input type="text" placeholder='الاسم والكنية' className='form-control p-3 mb-4' />
-                                <input type="email" placeholder="البريد الالكتروني" className='form-control p-3 mb-4' />
-                                <input type="number" placeholder="رقم الجوال" className='form-control p-3 mb-4' min="0" max="9" maxLength="10" />
-                                <input type="password" placeholder="كلمة السر" className='form-control p-3 mb-4' />
-                                <input type="password" placeholder="تأكيد كلمة السر" className='form-control p-3 mb-4' />
+                                <input
+                                    type="text"
+                                    placeholder='الاسم والكنية'
+                                    className={`form-control p-3 ${errors["firstAndLastName"] ? "border border-danger mb-2" : "mb-4"}`}
+                                    onChange={(e) => setFirstAndLastName(e.target.value)}
+                                />
+                                {errors["firstAndLastName"] && <p className='error-msg text-danger'>{ errors["firstAndLastName"] }</p>}
+                                <input
+                                    type="text"
+                                    placeholder="البريد الالكتروني"
+                                    className={`form-control p-3 ${errors["email"] ? "border border-danger mb-2" : "mb-4"}`}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                {errors["email"] && <p className='error-msg text-danger'>{ errors["email"] }</p>}
+                                <input
+                                    type="number"
+                                    placeholder="رقم الجوال"
+                                    className={`form-control p-3 ${errors["mobilePhone"] ? "border border-danger mb-2" : "mb-4"}`}
+                                    onChange={(e) => setMobilePhone(e.target.value)}
+                                />
+                                {errors["mobilePhone"] && <p className='error-msg text-danger'>{ errors["mobilePhone"] }</p>}
+                                <input
+                                    type="password"
+                                    placeholder="كلمة السر"
+                                    className={`form-control p-3 ${errors["password"] ? "border border-danger mb-2" : "mb-4"}`}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                {errors["password"] && <p className='error-msg text-danger'>{ errors["password"] }</p>}
+                                <input
+                                    type="password"
+                                    placeholder="تأكيد كلمة السر"
+                                    className='form-control p-3 mb-4'
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                />
                             </div>
                             {/* End Column */}
                             {/* Start Column */}
                             <div className="col-md-6">
-                                <select className='form-control p-3 mb-4'>
+                                <select
+                                    className='form-control p-3 mb-4'
+                                    onChange={(e) => setGender(e.target.value)}
+                                >
                                     <option defaultValue="" hidden>اختر الجنس</option>
                                     <option value="male">ذكر</option>
                                     <option value="female">أنثى</option>
                                 </select>
-                                <input type="date" placeholder="تاريخ الميلاد" className='form-control p-3 mb-4' />
-                                <select className='form-control p-3 mb-4'>
+                                <input
+                                    type="date"
+                                    placeholder="تاريخ الميلاد"
+                                    className='form-control p-3 mb-4'
+                                    onChange={(e) => setBirthday(e.target.value)}
+                                />
+                                <select
+                                    className='form-control p-3 mb-4'
+                                    onChange={(e) => setCity(e.target.value)}
+                                >
                                     <option defaultValue="" hidden>اختر المحافظة</option>
                                     <option value="lattakia">اللاذقية</option>
-                                    <option value="lattakia">حلب</option>
-                                    <option value="lattakia">دمشق</option>
-                                    <option value="lattakia">طرطوس</option>
-                                    <option value="lattakia">درعا</option>
-                                    <option value="lattakia">القنيطرة</option>
-                                    <option value="lattakia">ريف دمشق</option>
-                                    <option value="lattakia">حماة</option>
-                                    <option value="lattakia">إدلب</option>
-                                    <option value="lattakia">حمص</option>
-                                    <option value="lattakia">الحسكة</option>
-                                    <option value="lattakia">دير الزور</option>
-                                    <option value="lattakia">الرقة</option>
+                                    <option value="aleppo">حلب</option>
+                                    <option value="damascus">دمشق</option>
+                                    <option value="tartous">طرطوس</option>
+                                    <option value="daraa">درعا</option>
+                                    <option value="kenitra">القنيطرة</option>
+                                    <option value="rif-Damascus">ريف دمشق</option>
+                                    <option value="hams">حماة</option>
+                                    <option value="idlib">إدلب</option>
+                                    <option value="homs">حمص</option>
+                                    <option value="al-Hasakah">الحسكة</option>
+                                    <option value="deer-al-zour">دير الزور</option>
+                                    <option value="raqqa">الرقة</option>
                                 </select>
-                                <textarea placeholder="العنوان بالتفصيل - مثال: شارع ميسلون, في البناء مقابل محل انكو, في الطابق الرابع" className='form-control p-3 address' />
+                                <textarea
+                                    placeholder="العنوان بالتفصيل - مثال: شارع ميسلون, في البناء مقابل محل انكو, في الطابق الرابع"
+                                    className='form-control p-3 address'
+                                    onChange={(e) => setAddress(e.target.value)}
+                                />
                             </div>
                             {/* End Column */}
                         </div>
