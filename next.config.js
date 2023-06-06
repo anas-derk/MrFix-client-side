124490b9f62d9f108bb37b136138c73d31e0dffc
anas derk <anas.derk2020@gmail.com>
2023-06-06 07:58:09 -0700

    Edit BASE API URL And Config Domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,32 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    BASE_API_URL: "http://localhost:4000",
+    BASE_API_URL: "https://api.mr-fix2023.com",
+  },
+  async headers() {
+    return [
+      {
+        source: "//api.mr-fix2023.com/(.*)",
+        // source: "//localhost:4000/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://mr-fix2023.com",
+            // value: "http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ]
+      }
+    ];
   }
 }
 
