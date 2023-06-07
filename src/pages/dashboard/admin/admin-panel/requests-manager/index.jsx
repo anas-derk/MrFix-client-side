20f5aeaf9ba4a3dfddeb10a395e6b58467ce529e
anas derk <anas.derk2020@gmail.com>
2023-06-07 08:08:39 -0700

    Programming Requests Displaying

--- src/pages/dashboard/admin/admin-panel/requests-manager/index.jsx
+++ src/pages/dashboard/admin/admin-panel/requests-manager/index.jsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import Axios from "axios";
 
-const RequestsManager = () => {
+const RequestsManager = ({ result }) => {
     const router = useRouter();
+    const [isWaitStatus, setIsWaitStatus] = useState(false);
+    const [requestsData, setRequestsData] = useState([]);
+    const [errorMsg, setErrorMsg] = useState("");
     useEffect(() => {
         let adminId = localStorage.getItem("mr-fix-admin-id");
         if (!adminId) {
@@ -29,15 +31,76 @@ const RequestsManager = () => {
                 <title>مستر فيكس - إدارة الطلبات</title>
             </Head>
             {/* Start Content Section */}
-            <section className="content d-flex justify-content-center align-items-center flex-column text-center">
-                <h1 className="welcome-msg mb-4">مرحباً بك في صفحة إدارة الطلبات الخاصة بك في مستر فيكس</h1>
-                <Link className="btn btn-success request-manager-link w-25 mx-auto mb-4" href="/dashboard/admin/admin-panel/requests-manager">إدارة الطلبات</Link>
-                <Link className="btn btn-success manager-link w-25 mx-auto mb-4" href="/dashboard/admin/admin-panel/passwords-reset">إعادة تعيين كلمات السر</Link>
+            <section className="content text-center pt-5 pb-5">
+                {/* Start Container Component From Bootstrap */}
+                <div className="container">
+                    <h1 className="welcome-msg mb-4">مرحباً بك في صفحة إدارة الطلبات الخاصة بك في مستر فيكس</h1>
+                    <hr />
+                    {result.length > 0 ? result.map((request, index) =>
+                        <>
+                            {/* Start Request Details Box */}
+                            <div className="request-details-box mb-5" key={request._id}>
+                                <h5 className="mb-4">معلومات الطلب {index + 1}</h5>
+                                <table className="requests-table w-100">
+                                    <tbody>
+                                        <tr>
+                                            <td className="fw-bold p-3">نوع الطلب</td>
+                                            <td className="p-3">{request.requestType}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">نوع الخدمة</td>
+                                            <td className="p-3">{request.serviceType}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">العنوان الجديد</td>
+                                            <td className="p-3">{request.newAddress}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">تواريخ الأيام المفضلة</td>
+                                            <td className="p-3">{request.preferredDateOfVisit}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">الأوقاتا المفضلة للزيارة</td>
+                                            <td className="p-3">{request.preferredTimeOfVisit}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">أوقات الكهرباء النظامية</td>
+                                            <td className="p-3">{request.electricityTimes}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">هل يوجد طاقة بديلة ؟</td>
+                                            <td className="p-3">{request.isAlternativeEnergyExist}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold">Files</td>
+                                            {request.files.length > 0 ? <td>
+                                                {request.files.map((path, index) =>
+                                                    <a href={`${process.env.BASE_API_URL}/${path}`} target="_blank" className="d-block btn btn-success mb-3" key={index}>تحميل الصورة {index + 1}</a>
+                                                )}
+                                            </td> : <td>عذراً لا يوجد أي صور</td>}
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            {/* End Request Details Box */}
+                            <hr />
+                        </>
+                    ) : <p className="alert alert-danger w-50 mx-auto">عذراً لا يوجد أي طلبات حالياً !!</p>}
+                </div>
+                {/* End Container Component From Bootstrap */}
             </section>
             {/* End Content Section */}
         </div>
         // End Requests Manager Page
     );
+}
+
+export async function getServerSideProps() {
+    let res = await Axios.get(`${process.env.BASE_API_URL}/requests/all-requests`);
+    let result = await res.data;
+    return {
+        props: { result },
+    };
 }
 
 export default RequestsManager;
