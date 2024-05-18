253a67db60c6a47ddd6e627f2e7224fa8f480263
anas derk <anas.derk2020@gmail.com>
2024-05-18 04:47:35 +0300

    Start Fix Bugs

--- next.config.js
+++ next.config.js
@@ -1,21 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: true,
   env: {
-    // BASE_API_URL: "http://localhost:4000",
-    BASE_API_URL: "https://api.mr-fix2023.com",
+    BASE_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://api.mr-fix2023.com",
   },
   async headers() {
     return [
       {
-        // source: "//localhost:4000/(.*)",
-        source: "//api.mr-fix2023.com/(.*)",
+        source: process.env.NODE_ENV === "development" ? "//localhost:4000/(.*)" : "//api.mr-fix2023.com/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            // value: "http://localhost:3000",
-            value: "https://mr-fix2023.com",
+            value: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mr-fix2023.com",
           },
           {
             key: "Access-Control-Allow-Methods",
@@ -32,4 +30,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
