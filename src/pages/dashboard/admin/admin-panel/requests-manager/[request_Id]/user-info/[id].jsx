27164a1b0ff72cde13461ffbd4f5b07c0f036c7d
anas derk <anas.derk2020@gmail.com>
2024-05-23 20:54:50 +0300

    Continue With Fix

--- src/pages/dashboard/admin/admin-panel/requests-manager/[request_Id]/user-info/[id].jsx
+++ src/pages/dashboard/admin/admin-panel/requests-manager/[request_Id]/user-info/[id].jsx
@@ -1,80 +1,113 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Axios from "axios";
+import axios from "axios";
+import LoaderPage from "@/components/LoaderPage";
+import ErrorOnLoadingThePage from "@/components/ErrorOnLoadingThePage";
+import { getAdminInfo } from "../../../../../../../../public/global_functions/popular";
 
-const RequestSenderInfo = ({ result }) => {
+const RequestSenderInfo = ({ requestId, userId }) => {
+    const [isLoadingPage, setIsLoadingPage] = useState(true);
+    const [isErrorMsgOnLoadingThePage, setIsErrorMsgOnLoadingThePage] = useState(false);
+    const [requestSenderInfo, setRequestSenderInfo] = useState({});
     const router = useRouter();
     useEffect(() => {
-        let adminId = localStorage.getItem("mr-fix-admin-id");
-        if (!adminId) {
-            router.push("/dashboard/admin/login");
-        } else {
-            Axios.get(`${process.env.BASE_API_URL}/admin/admin-info/${adminId}`)
-                .then((res) => {
-                    let result = res.data;
-                    if (result === "عذراً ، حساب المسؤول غير موجود") {
-                        localStorage.removeItem("mr-fix-admin-id");
-                        router.push("/dashboard/admin/login");
+        const adminToken = localStorage.getItem(process.env.adminTokenNameInLocalStorage);
+        if (adminToken) {
+            getAdminInfo()
+                .then(async (result) => {
+                    if (result.error) {
+                        localStorage.removeItem(process.env.adminTokenNameInLocalStorage);
+                        await router.replace("/dashboard/admin/login");
+                    } else {
+                        result = await getRequestSenderInfo(requestId, userId);
+                        setRequestSenderInfo(result.data);
+                        setIsLoadingPage(false);
                     }
                 })
-                .catch((err) => console.log(err));
-        }
+                .catch(async (err) => {
+                    if (err?.response?.data?.msg === "Unauthorized Error") {
+                        localStorage.removeItem(process.env.adminTokenNameInLocalStorage);
+                        await router.replace("/dashboard/admin/login");
+                    }
+                    else {
+                        setIsLoadingPage(false);
+                        setIsErrorMsgOnLoadingThePage(true);
+                    }
+                });
+        } else router.push("/dashboard/admin/login");
     }, []);
+    const getRequestSenderInfo = async (requestId, userId) => {
+        try {
+            const res = await axios.get(`${process.env.BASE_API_URL}/requests/request-sender-info?requestId=${requestId}&userId=${userId}`, {
+                headers: {
+                    Authorization: localStorage.getItem(process.env.adminTokenNameInLocalStorage)
+                }
+            });
+            return res.data;
+        }
+        catch (err) {
+            throw Error(err);
+        }
+    }
     return (
         // Start Request Sender Info Page
         <div className="request-sender-info">
             <Head>
                 <title>مستر فيكس - معلومات الشخص الذي أرسل الطلب </title>
             </Head>
-            {/* Start Content Section */}
-            <section className="content text-center pt-5 pb-5">
-                {/* Start Container Component From Bootstrap */}
-                <div className="container">
-                    <h1 className="welcome-msg mb-4">مرحباً بك في صفحة معلومات الشخص الذي أرسل الطلب</h1>
-                    <hr />
-                    {result !== "عذراً ، لا يوجد طلب بهذا المعرّف !!!" ?
-                        /* Start Request Sender Details Box */
-                        <div className="request-sender-details-box" key={result._id}>
-                            <table className="request-sender-info-table w-100 mt-4">
-                                <tbody>
-                                    <tr>
-                                        <td className="fw-bold p-3">الاسم والكنية</td>
-                                        <td className="p-3">{result.firstAndLastName}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="fw-bold p-3">البريد الالكتروني</td>
-                                        <td className="p-3">{result.email}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="fw-bold p-3">رقم الموبايل</td>
-                                        <td className="p-3">{result.mobilePhone}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="fw-bold p-3">الجنس</td>
-                                        <td className="p-3">{result.gender}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="fw-bold p-3">تاريخ الميلاد</td>
-                                        <td className="p-3">{result.birthday}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="fw-bold p-3">المدينة</td>
-                                        <td className="p-3">{result.city}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="fw-bold p-3">العنوان</td>
-                                        <td className="p-3">{result.address}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        /* End Request Sender Details Box */
-                        : <p className="alert alert-danger w-50 mx-auto">عذراً هذا المستخدم لم يرسل طلباً !!</p>}
-                </div>
-                {/* End Container Component From Bootstrap */}
-            </section>
-            {/* End Content Section */}
+            {!isLoadingPage && !isErrorMsgOnLoadingThePage && <>
+                {/* Start Content Section */}
+                <section className="content text-center pt-5 pb-5">
+                    {/* Start Container Component From Bootstrap */}
+                    <div className="container">
+                        <h1 className="welcome-msg mb-4">مرحباً بك في صفحة معلومات الشخص الذي أرسل الطلب</h1>
+                        <hr />
+                        {Object.keys(requestSenderInfo).length > 0 ?
+                            /* Start Request Sender Details Box */
+                            <div className="request-sender-details-box" key={requestSenderInfo._id}>
+                                <table className="request-sender-info-table w-100 mt-4">
+                                    <tbody>
+                                        <tr>
+                                            <td className="fw-bold p-3">الاسم والكنية</td>
+                                            <td className="p-3">{requestSenderInfo.firstAndLastName}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">البريد الالكتروني</td>
+                                            <td className="p-3">{requestSenderInfo.email}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">رقم الموبايل</td>
+                                            <td className="p-3">{requestSenderInfo.mobilePhone}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">الجنس</td>
+                                            <td className="p-3">{requestSenderInfo.gender}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">تاريخ الميلاد</td>
+                                            <td className="p-3">{requestSenderInfo.birthday}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">المدينة</td>
+                                            <td className="p-3">{requestSenderInfo.city}</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="fw-bold p-3">العنوان</td>
+                                            <td className="p-3">{requestSenderInfo.address}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            /* End Request Sender Details Box */
+                            : <p className="alert alert-danger w-50 mx-auto">عذراً هذا المستخدم لم يرسل طلباً !!</p>}
+                    </div>
+                    {/* End Container Component From Bootstrap */}
+                </section>
+                {/* End Content Section */}
+            </>}
+            {isLoadingPage && !isErrorMsgOnLoadingThePage && <LoaderPage />}
+            {isErrorMsgOnLoadingThePage && <ErrorOnLoadingThePage />}
         </div>
         // End Request Sender Info Page
     );
@@ -83,10 +116,11 @@ const RequestSenderInfo = ({ result }) => {
 export async function getServerSideProps(context) {
     const requestId = context.params.request_Id,
         userId = context.params.id;
-    let res = await Axios.get(`${process.env.BASE_API_URL}/admin/requests/${requestId}/users/${userId}`);
-    let result = await res.data;
     return {
-        props: { result },
+        props: {
+            requestId,
+            userId
+        },
     };
 }
 
--- src/pages/dashboard/admin/admin-panel/requests-manager/index.jsx
+++ src/pages/dashboard/admin/admin-panel/requests-manager/index.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { getAdminInfo, getAllRequestsInsideThePage, getRequestsCount } from "../../../../../../public/global_functions/popular";
+import LoaderPage from "@/components/LoaderPage";
+import ErrorOnLoadingThePage from "@/components/ErrorOnLoadingThePage";
 
 const RequestsManager = ({ result }) => {
     const [isLoadingPage, setIsLoadingPage] = useState(true);
@@ -63,81 +65,85 @@ const RequestsManager = ({ result }) => {
             <Head>
                 <title>مستر فيكس - إدارة الطلبات</title>
             </Head>
-            {/* Start Content Section */}
-            <section className="content text-center pt-5 pb-5">
-                {/* Start Container Component From Bootstrap */}
-                <div className="container">
-                    <h1 className="welcome-msg mb-4">مرحباً بك في صفحة إدارة الطلبات الخاصة بك في مستر فيكس</h1>
-                    <hr />
-                    {allRequestsInsideThePage.length > 0 ? allRequestsInsideThePage.map((request, index) =>
-                        <>
-                            {/* Start Request Details Box */}
-                            <div className="request-details-box mb-5" key={request._id}>
-                                <h5 className="mb-4">معلومات الطلب {index + 1}</h5>
-                                <table className="requests-table w-100">
-                                    <tbody>
-                                        <tr>
-                                            <td className="fw-bold p-3">تاريخ وساعة الطلب</td>
-                                            <td className="p-3">{getDateFormated(request.requestPostDate)}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3">نوع الطلب</td>
-                                            <td className="p-3">{request.requestType}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3">نوع الخدمة</td>
-                                            <td className="p-3">{request.serviceType}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3">شرح الطلب  / العنوان الجديد</td>
-                                            <td className="p-3">{request.explainAndNewAddress}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3">تواريخ الأيام المفضلة</td>
-                                            <td className="p-3">{request.preferredDateOfVisit}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3">الأوقاتا المفضلة للزيارة</td>
-                                            <td className="p-3">{request.preferredTimeOfVisit}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3">أوقات الكهرباء النظامية</td>
-                                            <td className="p-3">{request.electricityTimes}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3">هل يوجد طاقة بديلة ؟</td>
-                                            <td className="p-3">{request.isAlternativeEnergyExist}</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold">الصور</td>
-                                            {request.files.length > 0 ? <td>
-                                                {request.files.map((path, index) =>
-                                                    <a href={`${process.env.BASE_API_URL}/${path}`} target="_blank" className="d-block btn btn-success mb-3" key={index}>تحميل الصورة {index + 1}</a>
-                                                )}
-                                            </td> : <td>عذراً لا يوجد أي صور</td>}
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold p-3" colSpan="2">
-                                                <Link
-                                                    href={`/dashboard/admin/admin-panel/requests-manager/${request._id}/user-info/${request.userId}`}
-                                                    className="btn btn-success p-3"
-                                                    target="_blank"
-                                                >
-                                                    معلومات الشخص الذي أرسل الطلب
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            {/* End Request Details Box */}
-                            <hr />
-                        </>
-                    ) : <p className="alert alert-danger w-50 mx-auto">عذراً لا يوجد أي طلبات حالياً !!</p>}
-                </div>
-                {/* End Container Component From Bootstrap */}
-            </section>
-            {/* End Content Section */}
+            {!isLoadingPage && !isErrorMsgOnLoadingThePage && <>
+                {/* Start Content Section */}
+                <section className="content text-center pt-5 pb-5">
+                    {/* Start Container Component From Bootstrap */}
+                    <div className="container">
+                        <h1 className="welcome-msg mb-4">مرحباً بك في صفحة إدارة الطلبات الخاصة بك في مستر فيكس</h1>
+                        <hr />
+                        {allRequestsInsideThePage.length > 0 ? allRequestsInsideThePage.map((request, index) =>
+                            <>
+                                {/* Start Request Details Box */}
+                                <div className="request-details-box mb-5" key={request._id}>
+                                    <h5 className="mb-4">معلومات الطلب {index + 1}</h5>
+                                    <table className="requests-table w-100">
+                                        <tbody>
+                                            <tr>
+                                                <td className="fw-bold p-3">تاريخ وساعة الطلب</td>
+                                                <td className="p-3">{getDateFormated(request.requestPostDate)}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3">نوع الطلب</td>
+                                                <td className="p-3">{request.requestType}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3">نوع الخدمة</td>
+                                                <td className="p-3">{request.serviceType}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3">شرح الطلب  / العنوان الجديد</td>
+                                                <td className="p-3">{request.explainAndNewAddress}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3">تواريخ الأيام المفضلة</td>
+                                                <td className="p-3">{request.preferredDateOfVisit}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3">الأوقاتا المفضلة للزيارة</td>
+                                                <td className="p-3">{request.preferredTimeOfVisit}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3">أوقات الكهرباء النظامية</td>
+                                                <td className="p-3">{request.electricityTimes}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3">هل يوجد طاقة بديلة ؟</td>
+                                                <td className="p-3">{request.isAlternativeEnergyExist}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold">الصور</td>
+                                                {request.files.length > 0 ? <td>
+                                                    {request.files.map((path, index) =>
+                                                        <a href={`${process.env.BASE_API_URL}/${path}`} target="_blank" className="d-block btn btn-success mb-3" key={index}>تحميل الصورة {index + 1}</a>
+                                                    )}
+                                                </td> : <td>عذراً لا يوجد أي صور</td>}
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-bold p-3" colSpan="2">
+                                                    <Link
+                                                        href={`/dashboard/admin/admin-panel/requests-manager/${request._id}/user-info/${request.userId}`}
+                                                        className="btn btn-success p-3"
+                                                        target="_blank"
+                                                    >
+                                                        معلومات الشخص الذي أرسل الطلب
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                {/* End Request Details Box */}
+                                <hr />
+                            </>
+                        ) : <p className="alert alert-danger w-50 mx-auto">عذراً لا يوجد أي طلبات حالياً !!</p>}
+                    </div>
+                    {/* End Container Component From Bootstrap */}
+                </section>
+                {/* End Content Section */}
+                {isLoadingPage && !isErrorMsgOnLoadingThePage && <LoaderPage />}
+                {isErrorMsgOnLoadingThePage && <ErrorOnLoadingThePage />}
+            </>}
         </div>
         // End Requests Manager Page
     );
